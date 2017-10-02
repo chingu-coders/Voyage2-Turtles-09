7781e94979cef334bc6e5c19129f6ba8e7c61823
David Lars Ketch <davidlarsketch@gmail.com>
2017-10-02 13:48:36 -0500

    Initial test functions for printing greeting & user name

--- js/script-name-greet.js
+++ js/script-name-greet.js
@@ -1,1 +1,24 @@
 "use strict"
+
+window.onload = (() => {
+  let currentHour, greeting, userName;
+
+  currentHour = new Date().getHours();
+  console.log(currentHour);
+
+  if (currentHour > 2 && currentHour < 12) {
+    greeting = "Morning, "
+  } else if (currentHour < 18){
+    greeting = "Afternoon, "
+  } else {
+    greeting = "Evening, "
+  }
+  console.log(greeting);
+
+  userName = "test user";
+  console.log(userName);
+
+  const userNameHTMLId = document.getElementById("greeting");
+  console.log(userNameHTMLId);
+  userNameHTMLId.innerHTML = "Good " + greeting + userName + ".";
+})
