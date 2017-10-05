d5f0479215f6a54cfbd645a8579599749e9fb32f
Jordan Leslie <this.codedev@gmail.com>
2017-10-05 15:06:59 -0500

    Fix issue where photo location causes an error if no location is provided

--- background.js
+++ background.js
@@ -4,25 +4,30 @@
     const api = "https://api.unsplash.com/photos/random";
     const appID = "51eeacda52a858956883fcd86384424f78d38da8fb4f8b61b65723e41223d6b1";
     const query = api + "?client_id=" + appID + "&collections=488341";
-    const topSlider = document.querySelector("#image-data");
-    const bottomSlider = document.querySelector("#image-location");
+    const imgPhotographer = document.querySelector("#image-photographer");
+    const imgLocation = document.querySelector("#image-location");
 
-    // fetch(query).then( response => response.json()).then( data =>
-    // {
-    //     // IMAGE DATA VARS
-    //     let bgUrl = data.urls.regular;
-    //     let photographer = data.user.name;
-    //     let imageLocation = data.user.location;
-    //     let imageDescription = data.description;
-    //
-    //     // SET BG
-    //     console.log(data);
-    //     document.body.style.background = `#f3f3f3 url('${bgUrl}') center center fixed / cover no-repeat`;
-    //
-    //     // IMAGE DATA
-    //     topSlider.innerHTML =`<p>${imageLocation}</p>`;
-    //     bottomSlider.innerHTML =`<p>${photographer}</p>`;
-    // });
+    fetch(query).then( response => response.json()).then( data =>
+    {
+        // IMAGE DATA VARS
+        let bgUrl = data.urls.regular;
+        let photographerData = data.user.name;
+        // let imageLocationData = data.location.name;
+        // let imageLocation = data.location.title;
+        let imageDescriptionData = data.description;
+        let username = data.user.username;
+        let linkToUser = `https://unsplash.com/@${username}?utm_source=over&utm_medium=referral&utm_campaign=api-credit`;
+        let imageLocationData = data.user.location || "Photo Credits";
+
+
+        // SET BG
+        console.log(data);
+        document.body.style.background = `#f3f3f3 url('${bgUrl}') center center fixed / cover no-repeat`;
+
+        // IMAGE DATA
+        imgLocation.innerHTML =`${imageLocationData}` || `{imageDescriptionData}`;
+        imgPhotographer.innerHTML =`<a href="${linkToUser}">${photographerData}</a>` || `<a href="{linkToUser}">${username}</a>`;
+    });
 
 
 
