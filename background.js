d694303e694fb4f380c4470ac5acdca981f7ed3f
Jordan Leslie <this.codedev@gmail.com>
2017-10-05 08:02:46 -0500

    Change temporary background image to something less ugly

--- background.js
+++ background.js
@@ -4,38 +4,32 @@
     const api = "https://api.unsplash.com/photos/random";
     const appID = "51eeacda52a858956883fcd86384424f78d38da8fb4f8b61b65723e41223d6b1";
     const query = api + "?client_id=" + appID + "&collections=488341";
-    const imageData = document.querySelector("#image-data");
+    const topSlider = document.querySelector("#image-data");
+    const bottomSlider = document.querySelector("#image-location");
+
+    // fetch(query).then( response => response.json()).then( data =>
+    // {
+    //     // IMAGE DATA VARS
+    //     let bgUrl = data.urls.regular;
+    //     let photographer = data.user.name;
+    //     let imageLocation = data.user.location;
+    //     let imageDescription = data.description;
+    //
+    //     // SET BG
+    //     console.log(data);
+    //     document.body.style.background = `#f3f3f3 url('${bgUrl}') center center fixed / cover no-repeat`;
+    //
+    //     // IMAGE DATA
+    //     topSlider.innerHTML =`<p>${imageLocation}</p>`;
+    //     bottomSlider.innerHTML =`<p>${photographer}</p>`;
+    // });
 
 
-
-
-    fetch(query).then( response => response.json()).then( data =>
-    {
-        // DATA VARS
-        let bgUrl = data.urls.regular;
-        let photographer = data.user.name;
-        let imageLocation = data.user.location;
-        let imageDescription = data.description;
-
-        // SET BG
-        console.log(data);
-        document.body.style.background = `#f3f3f3 url('${bgUrl}') center center fixed / cover no-repeat`;
-        imageData.innerHTML =`<p>${imageLocation}</p>`;
-        $( document ).ready(function (){
-            // IMAGE DATA
-            $("#slider").on("mouseover", function () {
-                console.log("I'm working");
-                $("#image-location").mouseOver().html(`<p>${photographer}</p>`);
-            });
-            $("#slider").on("mouseout", function () {
-                console.log("I'm out");
-                $("#image-location").slideUp();
-            });
+    $("document").ready(function () {
+        $("#slider").click(function () {
+            $("#slider").toggleClass("closed");
+        });
     });
-
-
-
-});
 
 
 
