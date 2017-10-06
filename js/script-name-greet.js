f2ac33131dc125cbc7ec278c024a10707396d439
David Lars Ketch <davidlarsketch@gmail.com>
2017-10-06 09:17:23 -0500

    Initial function for initial getting user name

--- js/script-name-greet.js
+++ js/script-name-greet.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
   //Development aids
-  localStorage.setItem("userName", "David");
+  //localStorage.setItem("userName", "David");
 
 // Stage A - Production code
 // Stage B - In development
@@ -54,6 +54,29 @@ window.onload = () => {
 
 //Stage C - initial ideas
 //Function for initial prompt asking for user name.
+const nameEntryGreetingHTMLId = document.getElementById("nameEntryGreeting");
+nameEntryGreetingHTMLId.innerHTML = "Hello, how are you this " + greeting + "?";
 
+const nameEntryLineHTMLId = document.getElementById("nameEntryLine");
+
+if (storedUserName) {
+  $(".initial-wrapper").css("display", "none");
+} else {
+  $(".main-wrapper").css("display", "none");
+}
+
+nameEntryLineHTMLId.addEventListener("keydown", (event) => {
+  if (event.which === 13) {
+    event.preventDefault();
+    if (nameEntryLineHTMLId.innerHTML) {
+      document.activeElement.blur();
+      newUserName = nameEntryLineHTMLId.innerHTML;
+      localStorage.setItem("userName", newUserName);
+      $(".initial-wrapper").fadeOut("slow", () => {
+        $(".main-wrapper").fadeIn("slow");
+      });
+    }
+  }
+});
 
 }
