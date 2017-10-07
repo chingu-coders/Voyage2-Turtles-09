e1f19b4d31b80c6667e1f9cd628745f395bbfcd9
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2017-10-07 21:03:36 +0100

    user can toggle focus between complete and incomplete. Status persists between tabs

--- js/script.js
+++ js/script.js
@@ -7,29 +7,68 @@
   const focusOutWrapper = document.querySelector("#focus-output-wrapper");
   const focusInWrapper = document.querySelector("#focus-input-wrapper");
   const focusClose = document.querySelector(".focus-close");
+  const focusUncheckedBox = document.querySelector(".fa-square-o");
+  const focusCheckedBox = document.querySelector(".fa-check-square-o");
   let focusText;
+  let currentCheckbox = focusUncheckedBox;
 
   getFocus();
 
   focusOutWrapper.addEventListener("mouseover", function(event) {
     showElement(focusClose);
+    showElement(currentCheckbox);
   });
 
   focusOutWrapper.addEventListener("mouseleave", function(event) {
     hideElement(focusClose);
+    hideElement(currentCheckbox);
   });
 
-  focusClose.addEventListener("click", function(event) {
+  focusCheckedBox.addEventListener("click", function() {
+    toggleFocusCompleted();
+  });
+
+  focusUncheckedBox.addEventListener("click", function() {
+    toggleFocusCompleted();
+  });
+
+  focusClose.addEventListener("click", function() {
     deleteFocus();
   });
 
+  function toggleFocusCompleted() {
+    if (currentCheckbox === focusUncheckedBox) {
+      currentCheckbox = focusCheckedBox;
+      showElement(focusCheckedBox);
+      hideElement(focusUncheckedBox);
+      focusOutput.classList.add("focus-completed");
+      chrome.storage.sync.set({"focusCompleted": true});
+    }
+    else {
+      currentCheckbox = focusUncheckedBox;
+      showElement(focusUncheckedBox);
+      hideElement(focusCheckedBox);
+      focusOutput.classList.remove("focus-completed");
+      chrome.storage.sync.set({"focusCompleted": false});
+    }
+  }
+
   function getFocus() {
     /* Is there already a focus in storage? */
-    chrome.storage.sync.get("focus", function(data){
+    chrome.storage.sync.get(null, function(obj){ // null gets whole object
       /* if there is, display it */    
-      if (data.focus !== undefined) {
+      if (obj.focus !== undefined) {
+        /* check if focus has been completed */
+        if (obj.focusCompleted === true) {
+          focusOutput.classList.add("focus-completed");
+          currentCheckbox = focusCheckedBox;
+        }
+        /* TODO figure out if the else statement is needed */
+        else {
+          focusOutput.classList.remove("focus-completed");
+        }
         showElement(focusOutWrapper);
-        displayFocus(data.focus);
+        displayFocus(obj.focus);
       }
       /* if not, get focus from user */
       else {
@@ -47,9 +86,9 @@
           focusText = this.value;
           this.value = "";
           displayFocus(focusText) 
-          chrome.storage.sync.set({"focus": focusText}, function() {
+          chrome.storage.sync.set({"focus": focusText, "focusCompleted": false, "focusTimestamp": Date.now()}, function() {
             /* TODO remove get and console.log, and figure out how to handle errors */
-            chrome.storage.sync.get(["focus"], function(data){
+            chrome.storage.sync.get(null, function(data){
               console.log(data);
             });
           });       
@@ -73,9 +112,13 @@
   }
 
   function deleteFocus() {
-    chrome.storage.sync.remove(["focus"]);
+    chrome.storage.sync.remove(["focus", "focusCompleted", "focusTimestamp"]);
     hideElement(focusOutWrapper);
     focusOutput.innerHTML = "";
+    focusOutput.classList.remove("focus-completed");
+    showElement(focusUncheckedBox);
+    hideElement(focusCheckedBox);
+    currentCheckbox = focusUncheckedBox;
     getFocus();
   }
 
