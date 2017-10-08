21b1f426099e48c84f6194b779818188ef71b627
David Lars Ketch <davidlarsketch@gmail.com>
2017-10-08 12:34:46 -0500

    Removes initial test functions

--- js/script-clock.js
+++ js/script-clock.js
@@ -3,13 +3,4 @@
 let current, timeStringToPrint, time12HourOptions, time24HourOptions, options, timeSetting;
 
 window.onload = () =>{
-  currentDate = new Date();
-  console.log(currentDate);
-  currentHour = currentDate.getHours();
-  currentMinute = currentDate.getMinutes();
-  currentTime = currentHour + ": " + currentMinute;
-  console.log(currentTime);
-
-  defaultLocaleTimeString = currentDate.toLocaleTimeString().split('').slice(0,4).join('');
-  console.log(stringDate);
 }
