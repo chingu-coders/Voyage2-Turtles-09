8a34606626c504ee2b2704c3b566859c2b184b39
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2017-10-09 22:44:23 +0100

    refactored code. Now storing focus data as an object

--- js/script.js
+++ js/script.js
@@ -1,79 +1,71 @@
 "use strict";
 
 /* Main Focus */
+
 (function() {
   const focusInput = document.querySelector("#focus-input");
   const focusOutput = document.querySelector("#focus-output");
   const focusOutWrapper = document.querySelector("#focus-output-wrapper");
   const focusInWrapper = document.querySelector("#focus-input-wrapper");
   const focusClose = document.querySelector(".focus-close");
+  const focusCheckbox = document.querySelector(".focus-checkbox");
   const focusUncheckedBox = document.querySelector(".fa-square-o");
   const focusCheckedBox = document.querySelector(".fa-check-square-o");
-  let focusText;
-  let currentCheckbox = focusUncheckedBox;
+
+  let focusObj = {
+    "focusText": "",
+    "focusCompleted": false,
+    "focusTimestamp": 0
+  };
 
   getFocus();
 
   focusOutWrapper.addEventListener("mouseover", function(event) {
     showElement(focusClose);
-    showElement(currentCheckbox);
+    showElement(focusCheckbox);
   });
 
   focusOutWrapper.addEventListener("mouseleave", function(event) {
     hideElement(focusClose);
-    hideElement(currentCheckbox);
-  });
-
-  focusCheckedBox.addEventListener("click", function() {
-    toggleFocusCompleted();
-  });
-
-  focusUncheckedBox.addEventListener("click", function() {
-    toggleFocusCompleted();
+    hideElement(focusCheckbox);
   });
 
   focusClose.addEventListener("click", function() {
     deleteFocus();
   });
 
-  function toggleFocusCompleted() {
-    if (currentCheckbox === focusUncheckedBox) {
-      currentCheckbox = focusCheckedBox;
-      showElement(focusCheckedBox);
-      hideElement(focusUncheckedBox);
-      focusOutput.classList.add("focus-completed");
-      chrome.storage.sync.set({"focusCompleted": true});
-    }
-    else {
-      currentCheckbox = focusUncheckedBox;
-      showElement(focusUncheckedBox);
-      hideElement(focusCheckedBox);
-      focusOutput.classList.remove("focus-completed");
-      chrome.storage.sync.set({"focusCompleted": false});
-    }
-  }
+  focusCheckedBox.addEventListener("click", function() {
+    toggleOffFocusCompleted();
+  });
+
+  focusUncheckedBox.addEventListener("click", function() {
+    toggleOnFocusCompleted();
+  });
 
   function getFocus() {
     /* Is there already a focus in storage? */
-    chrome.storage.sync.get(null, function(obj){ // null gets whole object
-      /* if there is, display it */    
-      if (obj.focus !== undefined) {
+    chrome.storage.sync.get("focusObj", function(obj){
+      /* if there is, display it */      
+      if (obj.focusObj !== undefined && obj !== {}) { //TODO test if this condition is sufficient
+        console.log(obj.focusObj.focusText);
+        focusObj = obj.focusObj;
         /* check if focus has been completed */
-        if (obj.focusCompleted === true) {
+        if (focusObj.focusCompleted === true) {
+          markAsComplete();
           focusOutput.classList.add("focus-completed");
-          currentCheckbox = focusCheckedBox;
+          console.log("focus completed");
         }
         /* TODO figure out if the else statement is needed */
         else {
           focusOutput.classList.remove("focus-completed");
         }
         showElement(focusOutWrapper);
-        displayFocus(obj.focus);
+        displayFocus(focusObj.focusText);
       }
       /* if not, get focus from user */
       else {
-        //console.log(data);
         showElement(focusInWrapper);
+        markAsIncomplete();
         getFocusFromUser();
       }
     });
@@ -83,10 +75,11 @@
     focusInput.addEventListener("keydown", function(event) {
       if (event.keyCode === 13){ //Enter key pressed
         if (this.value !== "") {
-          focusText = this.value;
+          focusObj.focusText = this.value;
+          focusObj.focusTimestamp = Date.now();
           this.value = "";
-          displayFocus(focusText) 
-          chrome.storage.sync.set({"focus": focusText, "focusCompleted": false, "focusTimestamp": Date.now()}, function() {
+          displayFocus(focusObj.focusText) 
+          chrome.storage.sync.set({focusObj}, function() {
             /* TODO remove get and console.log, and figure out how to handle errors */
             chrome.storage.sync.get(null, function(data){
               console.log(data);
@@ -99,8 +92,40 @@
 
   function displayFocus(focusText) {
     hideElement(focusInWrapper);
-    focusOutput.innerHTML = focusText;
+    focusOutput.innerHTML = focusObj.focusText;
     showElement(focusOutWrapper);
+  }
+
+  function deleteFocus() {
+    chrome.storage.sync.remove("focusObj");
+    hideElement(focusOutWrapper);
+    focusOutput.innerHTML = "";
+    markAsIncomplete();
+    getFocus();
+  }
+
+  function toggleOnFocusCompleted() {
+    markAsComplete()
+    focusObj.focusCompleted = true;
+    chrome.storage.sync.set({focusObj});
+  }
+
+  function toggleOffFocusCompleted() {
+    markAsIncomplete();
+    focusObj.focusCompleted = false;
+    chrome.storage.sync.set({focusObj});
+  }
+
+  function markAsIncomplete() {
+    focusOutput.classList.remove("focus-completed");
+    showElement(focusUncheckedBox);
+    hideElement(focusCheckedBox);
+  }
+
+  function markAsComplete() {
+    focusOutput.classList.add("focus-completed");
+    showElement(focusCheckedBox);
+    hideElement(focusUncheckedBox);
   }
 
   function showElement(element) {
@@ -111,15 +136,6 @@
     element.classList.add("hidden");
   }
 
-  function deleteFocus() {
-    chrome.storage.sync.remove(["focus", "focusCompleted", "focusTimestamp"]);
-    hideElement(focusOutWrapper);
-    focusOutput.innerHTML = "";
-    focusOutput.classList.remove("focus-completed");
-    showElement(focusUncheckedBox);
-    hideElement(focusCheckedBox);
-    currentCheckbox = focusUncheckedBox;
-    getFocus();
-  }
-
 })();
+
+/* end of Main Focus */
