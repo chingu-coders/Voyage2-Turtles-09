3df9d44d8c8b5bd72179c5ae1592a32481ecf316
David Lars Ketch <davidlarsketch@gmail.com>
2017-10-10 11:35:08 -0500

    Adds function dblclickEventListener()

--- js/script-clock.js
+++ js/script-clock.js
@@ -41,6 +41,30 @@ function changeTime(){
   console.log(timeStringToPrint);
 }
 
+function dblclickEventListener(){
+  timeHTMLSelector.addEventListener("dblclick", () => {
+    chrome.storage.sync.get(null, (obj) => {
+      let error = chrome.runtime.lastError;
+      switch (true) {
+        case error:
+          console.error(error);
+          break;
+        case obj.timeSetting.hour12:
+          options = time24HourOptions;
+          changeTime(options);
+          break;
+        case !obj.timeSetting.hour12:
+          options = time12HourOptions;
+          changeTime(options);
+          break;
+        default:
+          options = time24HourOptions //Should be set to locale default
+          changeTime(options);
+      }
+    chrome.storage.sync.set({"timeSetting": options});
+  });
+});}
+
 $(document).ready(() => {
   pullTimeSetting();
   changeTime();
