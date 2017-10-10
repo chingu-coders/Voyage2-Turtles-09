ab7dc6dd69b719baa2d5c3cbb90c600df7bbc323
David Lars Ketch <davidlarsketch@gmail.com>
2017-10-10 11:45:59 -0500

    Removes window.onload; adds self-invoking function

--- js/script-clock.js
+++ js/script-clock.js
@@ -1,71 +1,73 @@
 "use strict";
-const timeHTMLSelector = document.querySelector("#time");
+(() => {
+  const timeHTMLSelector = document.querySelector("#time");
 
-const time24HourOptions = {
-  hour12: false,
-  hour: '2-digit',
-  minute: '2-digit'
-};
+  const time24HourOptions = {
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit'
+  };
 
-const time12HourOptions = {
-  hour12: true,
-  hour: '2-digit',
-  minute: '2-digit'
-};
+  const time12HourOptions = {
+    hour12: true,
+    hour: '2-digit',
+    minute: '2-digit'
+  };
 
-let options;
+  let options;
 
-function pullTimeSetting (){
-  chrome.storage.sync.get(null, (obj) => {
-    let error = chrome.runtime.lastError;
-    if (error) {
-      console.error(error);
-    } else if (!obj.timeSetting){
-      console.log(obj.timeSetting);
-      options = time24HourOptions //Should be set to locale default
-      changeTime(options);
-      dblclickEventListener()
-    } else {
-      console.log(obj.timeSetting);
-      options = obj.timeSetting;
-      changeTime(options);
-      dblclickEventListener();
-    }
-  });
-}
+  pullTimeSetting();
+  window.setInterval(pullTimeSetting, 5000);
 
-function changeTime(){
-  let current = new Date();
-  let timeStringToPrint = current.toLocaleTimeString([], options);
-  timeHTMLSelector.innerHTML = timeStringToPrint;
-  console.log(timeStringToPrint);
-}
-
-function dblclickEventListener(){
-  timeHTMLSelector.addEventListener("dblclick", () => {
+  function pullTimeSetting (){
     chrome.storage.sync.get(null, (obj) => {
       let error = chrome.runtime.lastError;
-      switch (true) {
-        case error:
-          console.error(error);
-          break;
-        case obj.timeSetting.hour12:
-          options = time24HourOptions;
-          changeTime(options);
-          break;
-        case !obj.timeSetting.hour12:
-          options = time12HourOptions;
-          changeTime(options);
-          break;
-        default:
-          options = time24HourOptions //Should be set to locale default
-          changeTime(options);
+      if (error) {
+        console.error(error);
+      } else if (!obj.timeSetting){
+        options = time24HourOptions //Should be set to locale default
+        changeTime(options);
+        dblclickEventListener()
+      } else {
+        options = obj.timeSetting;
+        changeTime(options);
+        dblclickEventListener();
       }
-    chrome.storage.sync.set({"timeSetting": options});
-  });
-});}
+    });
+  }
 
-$(document).ready(() => {
-  pullTimeSetting();
-  window.setInterval(changeTime, 30000);
-});
+  function changeTime(){
+    let current = new Date();
+    let timeStringToPrint = current.toLocaleTimeString([], options);
+    timeHTMLSelector.innerHTML = timeStringToPrint;
+  }
+
+  function dblclickEventListener(){
+    timeHTMLSelector.addEventListener("dblclick", () => {
+      chrome.storage.sync.get(null, (obj) => {
+        let error = chrome.runtime.lastError;
+        switch (true) {
+          case error:
+            console.error(error);
+            break;
+          case obj.timeSetting.hour12:
+            options = time24HourOptions;
+            changeTime(options);
+            break;
+          case !obj.timeSetting.hour12:
+            options = time12HourOptions;
+            changeTime(options);
+            break;
+          default:
+            options = time24HourOptions //Should be set to locale default
+            changeTime(options);
+        }
+      chrome.storage.sync.set({"timeSetting": options});
+    });
+  });}
+
+// $(document).ready(() => {
+//   pullTimeSetting();
+//   window.setInterval(changeTime, 30000);
+// });
+})();
