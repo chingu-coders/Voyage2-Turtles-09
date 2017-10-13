61dedb134395a3034d92c00ff05debed3fb4a0f2
Jordan Leslie <this.codedev@gmail.com>
2017-10-13 10:13:39 -0500

    Randomly select from a hard-coded json file of quotes

--- js/quote.js
+++ js/quote.js
@@ -1,16 +1,21 @@
 "use strict";
-console.log("Connected");
-let url = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?';
+// Json file of hard-coded quotes
+let url = 'https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520';
 let quote = document.getElementById("quote");
 let quoteLinks = document.getElementById("quoteLinks");
-let getQuote = (data) => {
-    quote.innerHTML = data.quoteText;
-    quoteLinks.innerHTML = "- " +(data.quoteAuthor || "Anonymous");
-};
 
+
+let getQuote = (data) => {
+    // Grabs a random quote from the json file
+    let quoteSelected = data[Math.floor(Math.random() * data.length)];
+    quote.innerHTML = quoteSelected.quote;
+    // If no author then 'Anonymous'
+    quoteLinks.innerHTML = "- " +(quoteSelected.name || "Anonymous");
+};
 let handleErr = (error) => console.warn(error);
 
-$.getJSON(url).done(getQuote).fail(handleErr);
+
+$.getJSON(url).then(data => getQuote(data)).fail(handleErr);
 
 
 
