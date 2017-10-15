d7ab56078f8c4b89d3fc69ecfd41c47f4e8e44c3
Jordan Leslie <this.codedev@gmail.com>
2017-10-15 08:45:10 -0500

    Refactor tweet and like button html to be included on index.html. Add tweet functionality. Append proxy to quote api

--- js/quote.js
+++ js/quote.js
@@ -1,22 +1,31 @@
 "use strict";
 // Json file of hard-coded quotes
-let url = 'https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520';
+// let url = 'https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520';
 let quote = document.getElementById("quote");
 let quoteLinks = document.getElementById("quoteLinks");
-
-
-let getQuote = (data) => {
-    // Grabs a random quote from the json file
-    let quoteSelected = data[Math.floor(Math.random() * data.length)];
-    quote.innerHTML = quoteSelected.quote;
-    // If no author then 'Anonymous'
-    quoteLinks.innerHTML = "- " +(quoteSelected.name || "Anonymous") +
-        '<button type="button" class="btn btn-default" aria-label="Left Align"><span class="glyphicon glyphicon-align-left" aria-hidden="true"></span></button>';
+let proxy2 = "https://cors.io/?";
+const twitter = 'https://twitter.com/intent/tweet?text=';
+let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": proxy2 + "https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",
+    "method": "GET",
 };
-let handleErr = (error) => console.warn(error);
 
+const handleErr = (error) => console.warn(error);
+const getQuote = (data) => {
+    quote.innerText = data.quoteText;
+    quoteLinks.prepend("- " + (data.quoteAuthor || 'Anonymous'));
+    return data;
 
-$.getJSON(url).then(data => getQuote(data)).fail(handleErr);
+};
+const getTweet = (data) => {
+    console.log("get tweet is running");
+    const tweet = () => window.open(twitter + quote.innerText.trim() + " - " + (data.quoteAuthor || 'Anonymous'), "_blank");
+    $("#tweet").on("click", tweet);
+};
+
+$.getJSON(settings).then(data => getQuote(data)).then(data => getTweet(data)).fail(handleErr);
 
 
 
