7a53d98aa6d8ec90a9b39d086dbba75f414dea4c
Jordan Leslie <this.codedev@gmail.com>
2017-10-19 07:21:15 -0500

    Add delete function

--- js/todo.js
+++ js/todo.js
@@ -3,17 +3,16 @@
 const todoBody = {
     listBody: document.getElementById("todo-list"),
     listItems: document.getElementsByClassName("checkbox"),
+    deleteIcons: document.getElementsByClassName("delete"),
     input: document.getElementsByTagName("input"),
 };
 
 
 //TODO: Make delete function
-
 let todo = {
-    taskArray: [],
     taskList: [],
     getInput:
-        function () {
+        function (){
             if(event.keyCode === 13) {
                 console.log(this.value);
                 todo.taskList.push(this.value);
@@ -21,19 +20,46 @@ let todo = {
             }
         },
     addTask:
-        function () {
+        function (){
             if(event.keyCode === 13) {
-                todoBody.listBody.innerHTML += "<li class='checkbox'>" + this.value + "</li>";
-                for (let i = 0; i < todoBody.listItems.length; i++) {
-                    todoBody.listItems[i].addEventListener("click", todo.checkTask, false);
-                }
+                // Build task item html here
+                todoBody.listBody.innerHTML +=
+                    "<li class='checkbox'>" + this.value + "</li>" +
+                    "<span class='delete'>(X)</span>";
+                todoUtil.applyCheck();
+                todoUtil.applyDelete(this);
             }
 
         },
-    checkTask:
-        function () {
-            this.classList.toggle("checked");
-    },
+    deleteTask:
+        function (){
+            let deleteIndex = todo.taskList.indexOf(this);
+            console.log("We're looking for the value " + this.value);
+            console.log("Delete index is: " + deleteIndex);
+            todo.taskList.splice(deleteIndex, 1);
+        }
+};
+
+let todoUtil = {
+    applyCheck:
+        function() {
+            // Need to loop through the node list and apply listener each time a task is added
+            // ApplyCheck is run at the end of todo.addTask
+            for (let i = 0; i < todoBody.listItems.length; i++) {
+                todoBody.listItems[i].addEventListener("click", function() {
+                    todoBody.listItems[i].classList.toggle("checked");
+                }, false);
+            }
+        },
+    applyDelete:
+        function(that) {
+            for (let i = 0; i < todoBody.deleteIcons.length; i++) {
+                let deleteButton = todoBody.deleteIcons[i];
+                console.log(that);
+                todo.deleteTask.apply(deleteButton);
+                todoBody.deleteIcons[i].addEventListener("click", todo.deleteTask, false);
+            }
+        }
 };
 
 (function (){
@@ -42,5 +68,6 @@ let todo = {
         todoBody.input[i].addEventListener("keydown", todo.getInput, false);
         todoBody.input[i].addEventListener("keydown", todo.addTask, false);
     }
-}());
+})();
+
 
