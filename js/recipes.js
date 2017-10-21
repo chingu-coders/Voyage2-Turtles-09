3828e583d894abd2a6d8a2395f30c34d7f53910f
Reuben Jones <mrreubenjones@gmail.com>
2017-10-21 09:44:37 -0700

    Break down URL and begin to grab data from api call

--- js/recipes.js
+++ js/recipes.js
@@ -1,12 +1,30 @@
-"use strict";
+(function recipes() {
+  "use strict";
 
+  // Build URL
+  const api = "https://api.edamam.com/search?q=";
+  let search = "chicken";
+  const app_id = "&app_id=373a2755";
+  const app_key = "&app_key=5e414263cb40da6abf1019a550333f43";
+  let options = "";
+  const url = api + search + app_id + app_key + options;
 
+  // Query Edamam API
+  $.getJSON(url, function(json) {
+    // Set random pick from data array
+    let randomRecipe = Math.round(Math.random() * 10);
 
+    // Set data variables
+    let title = json.hits[randomRecipe].recipe.label;
+    let calories = json.hits[randomRecipe].recipe.calories;
+    let source = json.hits[randomRecipe].recipe.source;
 
-let url = "https://api.edamam.com/search?q=chicken&app_id=373a2755&app_key=5e414263cb40da6abf1019a550333f43";
-$.getJSON(url, function(json) {
-    var title = json.hits[0].recipe.label;
+    console.log(json);
     console.log(title);
-});
+    console.log(calories);
+    console.log(source);
+  });
 
 
+})();
+// Recipes ends
