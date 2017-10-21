62e7108146226f7206bdf2e072cda46f9fa9343e
Reuben Jones <mrreubenjones@gmail.com>
2017-10-21 08:09:30 -0700

    Test api

--- js/recipes.js
+++ js/recipes.js
@@ -1,16 +1,12 @@
-    "use strict";
+"use strict";
 
 
 
-    $(function(){
-        var url = "https://api.edamam.com/search?q=chicken&app_id=${“373a2755”}&app_key=${“5e414263cb40da6abf1019a550333f43”}";
-        var quote = $("#quoteblock");// the id of the heading
-        $.get(url, function (data) {
-        var the_quote = data;
-        console.log(the_quote);
-        var author = $("#author");// id of author
-        author.text(the_quote.contents.quotes[0].author);
-        });
-    });
+
+let url = "https://api.edamam.com/search?q=chicken&app_id=373a2755&app_key=5e414263cb40da6abf1019a550333f43";
+$.getJSON(url, function(json) {
+    var title = json.hits[0].recipe.label;
+    console.log(title);
+});
 
 
