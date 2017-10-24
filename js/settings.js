c28f5cd708a47fcd43f258810c572599e763d1f2
toni@tonigibbons.co.uk <toni@tonigibbons.co.uk>
2017-10-24 01:11:56 +0100

    animated settings icon, completed settings about section

--- js/settings.js
+++ js/settings.js
@@ -3,18 +3,30 @@
 
   const settingsIcon = document.querySelector(".settings-icon");
   const settingsPanel = document.querySelector(".settings");
-  const extensionName = document.querySelector(".settingsAbout h1");
+  const aboutText = document.querySelector(".settings-about-text");
   const manifest = chrome.runtime.getManifest();
-console.log(manifest.version);
+  console.log(manifest.version);
 
   settingsIcon.addEventListener("click", toggleSettingsPanel);
 
   function toggleSettingsPanel() {
+    settingsIcon.classList.toggle("clicked");
     settingsPanel.classList.toggle("hidden");
   }
 
-  
-  extensionName.innerHTML = manifest.name;
+  aboutText.innerHTML = `
+    <h1>${manifest.name}</h1>
+    <p class="settings-version">Personal Dashboard <span>v${manifest.version}</span></p>
+    <p>Thank you for your support!</p>
+    <ul>
+      <li><a href="https://github.com/chingu-coders/Voyage2-Turtles-09">GitHub</a></li>
+      <li><a href="https://medium.com/chingu">Chingu</a></li>
+      <li><a href="">Website</a></li>
+      <li><a href="">Twitter</a></li>
+    </ul>
+    <footer>Made with <span class="fa fa-heart"></span> by Chingu developers</footer>
+  `;
+
 
 /********************
  * Utility Functions
