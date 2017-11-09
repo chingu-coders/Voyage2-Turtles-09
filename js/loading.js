cd6b7a4e9b1f067c724b70fb5cb224fbadc74664
David Lars Ketch <davidlarsketch@gmail.com>
2017-11-09 15:59:09 -0600

    Reformats backup background object

--- js/loading.js
+++ js/loading.js
@@ -1,27 +1,6 @@
 "use strict";
 
 (() => {
-  function backgroundBackup() {
-    let windowWidth = $(window).width();
-    let windowHeight = $(window).height();
-    let bgUrl = "url(";
-
-    const defaultBg = {
-      lowRes: {
-        url: "assets/default-bg-low-res.jpg",
-        width: 1250,
-        height: 834
-      },
-      midRes: {
-        url: "assets/default-bg-mid-res.jpg",
-        width: 2200,
-        height: 1469
-      },
-      highRes: {
-        url: "assets/default-bg-high-res.jpg",
-        width: 5120,
-        height: 3418
-      }
 
 //Backup quotes
   const backupQuotes = [
@@ -67,12 +46,32 @@
     }
   }];
 
-    function selectBgRes() {
   const storedData = {
     background: null,
     quote: null,
     recipe: null
   };
+
+  const defaultBg = {
+    lowRes: {
+      url: "assets/default-bg-low-res.jpg",
+      width: 1250,
+      height: 834
+    },
+    midRes: {
+      url: "assets/default-bg-mid-res.jpg",
+      width: 2200,
+      height: 1469
+    },
+    highRes: {
+      url: "assets/default-bg-high-res.jpg",
+      width: 5120,
+      height: 3418
+    },
+    selectBgRes: () => {
+      let windowWidth = $(window).width();
+      let windowHeight = $(window).height();
+
       switch (true) {
         case windowWidth <= defaultBg.lowRes.width || windowHeight <= defaultBg.lowRes.height:
           return defaultBg.lowRes.url;
@@ -84,11 +83,8 @@
           return defaultBg.highRes.url;
       }
     }
+  };
 
-    bgUrl += selectBgRes();
-    bgUrl += ") center center fixed / cover no-repeat";
-    console.log(bgUrl);
-    $(document.body).css("background", bgUrl);
   };
 
   backgroundBackup();
